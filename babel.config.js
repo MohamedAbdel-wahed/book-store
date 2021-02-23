4c8fe9ccacd4ad3c45e030bab60a0034542ce48d
MohamedAbdel-wahed <webdeveloper.int99@gmail.com>
2021-02-23 13:28:43 +0200

    add babel conig options to @babel/preset-env

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,14 @@ if(process.env.NODE_ENV !== "production"){
 
 module.exports = {
   presets: [
-    "@babel/preset-env",
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage", 
+        corejs: 3, 
+        targets: "defaults", 
+      }
+    ]
     [
       "@babel/preset-react",
       {
